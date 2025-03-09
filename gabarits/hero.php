5304cb72a3435e52d1cebe8209a0fe64352d4bd2
Mariana Neri Matos <168363391+e2396652-cmaisonneuve@users.noreply.github.com>
2025-03-09 18:47:28 -0400

    Updated hero with infos

--- gabarits/hero.php
+++ gabarits/hero.php
@@ -10,10 +10,16 @@
     <p class="hero__description">
         <?php bloginfo('description'); ?>
     </p>
+    <div class="hero__description">
+        <p>info@bougeottevoyages.com</p>
+        <p>514-123-4567<br>
+            1234 rue de la rue<br>
+            Montréal, QC H1H 1H1</p>
+        <p>Auteur: Mariana Neri Matos</p>
+    </div>
 
     <div class="hero__cta">
         <a href="" class="hero__button">Inscrivez-vous</a>
-
     </div>
     <div class="hero__social"><?php get_template_part('gabarits/icone-sociaux'); ?>
     </div>
