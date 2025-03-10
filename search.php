9ccccd53b969f0992f0965d553d7483dffa7546d
Mariana Neri Matos <168363391+e2396652-cmaisonneuve@users.noreply.github.com>
2025-03-10 19:12:29 -0400

    Fixed search.php

--- search.php
+++ search.php
@@ -20,26 +20,7 @@ get_header();
         <!-- Liste des résultats -->
         <?php if (have_posts()) : ?>
             <?php while (have_posts()) : the_post(); ?>
-                <article>
-                    <div class="card card--big">
-                        <div class="carte__image">
-                            <?php
-                            if (has_post_thumbnail()) {
-                                the_post_thumbnail('medium');
-                            }
-                            ?>
-                        </div>
-                        <div class="card__content">
-                            <a href="<?php the_permalink(); ?>">
-                                <h2 class="card__title"><?php the_title(); ?></h2>
-                            </a>
-                            <p class="card__description"><?php echo wp_trim_words(get_the_excerpt(), 100, ""); ?>
-                                <a href="<?php the_permalink(); ?>">[...] Continuer la lecture</a>
-                            </p>
-                        </div>
-                    </div>
-
-                </article>
+                <?php get_template_part('gabarits/carte'); ?>
             <?php endwhile; ?>
 
             <!-- Pagination -->
