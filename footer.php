5edcc0b3eb955e9426f67bd510c6a9cf3152f759
Mariana Neri Matos <168363391+e2396652-cmaisonneuve@users.noreply.github.com>
2025-03-10 19:00:54 -0400

    Fixed css footer

--- footer.php
+++ footer.php
@@ -1,16 +1,18 @@
+<?php $hero_telephone = get_theme_mod('hero_telephone', 'Default Title'); ?>
+<?php $hero_email = get_theme_mod('hero_email', 'Default Title'); ?>
 <footer>
     <div class="global flex">
         <section class="footer__about">
             <h4 class="footer__titre">Bougeotte Voyages</h4>
             <p class="footer__description"><?php bloginfo('description'); ?></p>
-            <p class="footer__courriel">info@bougeottevoyages.com</p>
-            <p class="footer__phone">514-123-4567</p>
+            <p class="footer__courriel"><?php echo $hero_email ?></p>
+            <p class="footer__phone"><?php echo $hero_telephone ?></p>
             <p class="footer__social"><?php get_template_part('gabarits/icone-sociaux'); ?>
             </p>
         </section>
         <section class="footer__about">
             <h4 class="footer__titre">Horaire</h4>
-            <p><strong>Lundi au vendredi</strong><br>
+            <p class="footer__horaire"><strong>Lundi au vendredi</strong><br>
                 8h00 à 17h00</p>
             <form class="recherche" role="search" method="get" action="<?php echo esc_url(home_url('/')); ?>">
                 <label>
@@ -23,7 +25,7 @@
         </section>
         <section class="footer__about">
             <h4 class="footer__titre">Categories</h4>
-            <div><?php wp_nav_menu(array('theme_location' => 'footer-menu', 'menu' => 'menu-principal', 'container' => 'div', 'container_class' => '', 'container_id' => '', 'container_aria_label' => '', 'menu_class' => '')) ?></div>
+            <div class="footer__categories"><?php wp_nav_menu(array('theme_location' => 'footer-menu', 'menu' => 'menu-principal', 'container' => 'div', 'container_class' => '', 'container_id' => '', 'container_aria_label' => '', 'menu_class' => '')) ?></div>
             </p>
         </section>
         <section class="footer__about">
