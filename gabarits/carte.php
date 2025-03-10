b9ab1d34da19f651a5bbc8c0c939c26285887691
Mariana Neri Matos <168363391+e2396652-cmaisonneuve@users.noreply.github.com>
2025-03-10 11:16:05 -0400

    Ajouter des champs personnalisés

--- gabarits/carte.php
+++ gabarits/carte.php
@@ -16,9 +16,11 @@
             <a href="<?php the_permalink(); ?>">
                 <h2 class="card__title"><?php the_title(); ?></h2>
             </a>
-            <p class="card__description"><?php echo wp_trim_words(get_the_excerpt(), 40, ""); ?>
+            <div class="carte__category"><?php the_category() ?></div>
+            <p class="card__description"><?php echo wp_trim_words(get_the_excerpt(), 25, ""); ?>
                 <a href="<?php the_permalink(); ?>">[...] Continuer la lecture</a>
             </p>
+            <p class="card__description">Température maximum: <?php the_field('temperature_maximum') ?> | minimum: <?php the_field('temperature_minimum') ?></p>
         </div>
     </div>
 
